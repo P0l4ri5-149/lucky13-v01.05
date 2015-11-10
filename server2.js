aeb477e357a68f03c7f5b6b738d8890fccb6ae81
Adam Rodriguez <adam.rodriguez765@gmail.com>
2015-11-09 18:59:17 -0800

    added changelog_ar and server2.js for training and changes
    See above data

---
+++ server2.js
@@ -0,0 +1,33 @@
+
+var port = process.env.PORT || 8083;  // set our port
+var express = require('express');
+var app = express();
+
+// ROUTES FOR OUR API
+// ========================================================
+var router = express.Router();
+
+// rest api methods: get, put, post, delete
+
+// test route to make sure everything is working (accessed at GET http://localhost:8083/api)
+router.get('/', function(req, res) {
+	res.json({ message: 'hooray! welcome to our api!'});
+});
+
+// test route to make sure everything is working (accessed at GET http://localhost:8083/api)
+router.get('/', function(req, res) {
+	res.json({ message: 'hooray! welcome to our api!'});
+});
+
+// REGISTER OUR HTML --------------------------
+// ALL OF OUR HTML FILES WILL BE DSERVED FROM THE HTML FOLDER
+app.use(express.static(__dirname + '/html'));
+
+// REGISTER OUR ROUTES ------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// =============================================================
+app.listen(port);
+console.log("Go to http://localhost:" + port);
+
